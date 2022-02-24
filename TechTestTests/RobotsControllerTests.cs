2eed096ec2d94cac6eab00ac26d5603111e0fd70
Angel Costela <angel.costela@docplanner.com>
2022-02-24 10:04:53 +0100

    test in red

--- TechTestTests/RobotsControllerTests.cs
+++ TechTestTests/RobotsControllerTests.cs
@@ -45,9 +45,9 @@ public class RobotsControllerTests
 
         //Set up test data
         var robotBuilder = new RobotBuilder();
-        Robot bloatyHeadRobot = robotBuilder.WithConditionExpertise("Bloaty Head").Build();
+        Robot bloatyHeadRobot = robotBuilder.WithId(1).WithConditionExpertise("Bloaty Head").Build();
         context.Robots.Add(bloatyHeadRobot);
-        context.Robots.Add(robotBuilder.WithConditionExpertise("Another").Build());
+        context.Robots.Add(robotBuilder.WithId(2).WithConditionExpertise("Another").Build());
         context.SaveChanges();
             
         var controller = new RobotsController(context);
@@ -55,10 +55,18 @@ public class RobotsControllerTests
         //Call controller and get result
         var result = controller.GetAvailable("Bloaty Head");
 
-        var expectedResult = new List<Robot>  { bloatyHeadRobot };
+        var expectedResult = new List<object>
+        {
+            new
+            {
+                Id = 1, 
+                conditionExpertise = "Bloaty Head"
+            }
+        };
 
         Assert.IsInstanceOfType(result, typeof(OkObjectResult));
-        Assert.AreEqual(expectedResult, ((OkObjectResult)result).Value);
+        // Assert.AreEqual(expectedResult, ((OkObjectResult)result).Value);
+        CollectionAssert.AreEqual(expectedResult, (List<object>)((OkObjectResult)result).Value);
 
         context.Database.EnsureDeleted();
         context.Dispose();
